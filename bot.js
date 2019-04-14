0788cacce224113c0df2a7d7985a74f3e5b80eac
msg <49604348+untafx@users.noreply.github.com>
2019-04-14 16:45:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,9 +14,9 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-    if (message.content === 'hi') {
+    if (message.content === '/news') {
 
-       message.reply('pong');
+       message.reply('Last News Update In 14/4/2019 : UntaFx has new post on @untafx Instagram!');
 
        }
 
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);
